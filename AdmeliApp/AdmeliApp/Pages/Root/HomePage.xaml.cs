1e2d09d908ba4d09718e7491b6036348075921bf
paul antezana <contacto@paulantezana.com>
2018-03-11 10:53:26 -0500

    new updates

--- AdmeliApp/AdmeliApp/Pages/Root/HomePage.xaml.cs
+++ AdmeliApp/AdmeliApp/Pages/Root/HomePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AdmeliApp.Helpers;
+using Microcharts;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +9,50 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace AdmeliApp.Pages.Root
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
-        List<Microcharts.Entry> entries = new List<Microcharts.Entry>
-        {
-            /*new Microcharts.Entry
-            {
-
-            };*/
-        };
+        private WebService webService = new WebService();
+        List<Entry> entries = new List<Entry>();
 
 		public HomePage ()
 		{
 			InitializeComponent ();
-		}
+            loadDataChart();
+        }
+
+        struct ultimasVentas
+        {
+            public string dia { get; set; }
+            public dynamic idVenta { get; set; }
+            public string total { get; set; }
+        }
+
+        private async void loadDataChart()
+        {
+            try
+            {
+                // localhost:8080/admeli/xcore/services.php/ventaspormes
+                List<ultimasVentas> ventas = await webService.GET<List<ultimasVentas>>("ventaspormes");
+                foreach (ultimasVentas item in ventas)
+                {
+                    entries.Add(new Entry(int.Parse(item.total))
+                    {
+                        Color = SKColor.Parse("#36A2EB"),
+                        Label = item.dia,
+                        ValueLabel = item.total,
+                    });
+                }
+                Chart1.Chart = new LineChart { Entries = entries };
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 	}
 }
--- AdmeliApp/AdmeliApp/ViewModel/MarcaViewModel.cs
+++ AdmeliApp/AdmeliApp/ViewModel/MarcaViewModel.cs
@@ -14,6 +14,54 @@ namespace AdmeliApp.ViewModel
     public class MarcaViewModel : BaseModel
     {
         internal WebService webService = new WebService();
+        internal Paginacion paginacion = new Paginacion(1, App.configuracionGeneral.itemPorPagina);
+
+        private int _CurrentPage;
+        public int CurrentPage
+        {
+            get { return this._CurrentPage; }
+            set
+            {
+                SetValue(ref this._CurrentPage, value);
+                this.paginacion.reloadPage((value == 0) ? 1 : value);
+                this.LoadMarca();
+            }
+        }
+
+        private string _NRegistros;
+        public string NRegistros
+        {
+            get { return this._NRegistros; }
+            set { SetValue(ref this._NRegistros, value); }
+        }
+
+        private bool _FirstIsVisible;
+        public bool FirstIsVisible
+        {
+            get { return this._FirstIsVisible; }
+            set { SetValue(ref this._FirstIsVisible, value); }
+        }
+
+        private bool _PreviousIsVisible;
+        public bool PreviousIsVisible
+        {
+            get { return this._PreviousIsVisible; }
+            set { SetValue(ref this._PreviousIsVisible, value); }
+        }
+
+        private bool _NextIsVisible;
+        public bool NextIsVisible
+        {
+            get { return this._NextIsVisible; }
+            set { SetValue(ref this._NextIsVisible, value); }
+        }
+
+        private bool _LastIsVisible;
+        public bool LastIsVisible
+        {
+            get { return this._LastIsVisible; }
+            set { SetValue(ref this._LastIsVisible, value); }
+        }
 
         #region =================================== COLLECTIONS ===================================
         private List<Marca> marcaList { get; set; }
@@ -52,7 +100,6 @@ namespace AdmeliApp.ViewModel
         public ICommand NuevoCommand =>
             _NuevoCommand ?? (_NuevoCommand = new Command(() => ExecuteNuevo()));
 
-
         private ICommand _FirstCommand;
         public ICommand FirstCommand =>
             _FirstCommand ?? (_FirstCommand = new Command(() => ExecuteFirstPage()));
@@ -73,17 +120,17 @@ namespace AdmeliApp.ViewModel
         #region =========================== CONSTRUCTOR ===========================
         public MarcaViewModel()
         {
-            instance = this;
-            CurrentMarca = new MarcaItemViewModel();
-            LoadMarca(1, 30);
+            MarcaViewModel.instance = this;
+            this.CurrentMarca = new MarcaItemViewModel();
+            this.LoadMarca();
         }
         #endregion
 
         #region =========================== COMMAND EXECUTE ===========================
         internal void ExecuteRefresh()
         {
-            MarcaItems.Clear();
-            LoadMarca(1, 30);
+            this.MarcaItems.Clear();
+            this.LoadMarca();
         }
 
         private void ExecuteSearch()
@@ -103,41 +150,65 @@ namespace AdmeliApp.ViewModel
 
         private void ExecuteNuevo()
         {
-            SetCurrentMarca(new MarcaItemViewModel() { Nuevo = true, DeleteIsEnabled =  false });
+            this.SetCurrentMarca(new MarcaItemViewModel() { Nuevo = true, DeleteIsEnabled =  false });
             App.MarcaPage.Navigation.PushAsync(new MarcaItemPage()); // Navegacion
         }
 
         private void ExecuteFirstPage()
         {
-            throw new NotImplementedException();
+            if (this.CurrentPage != 1)
+            {
+                this.paginacion.firstPage();
+                this.LoadMarca();
+            }
         }
 
         private void ExecutePreviousPage()
         {
-            throw new NotImplementedException();
+            if (this.CurrentPage != 1)
+            {
+                this.paginacion.previousPage();
+                this.LoadMarca();
+            }
         }
 
         private void ExecuteNextPage()
         {
-            throw new NotImplementedException();
+            if (this.paginacion.pageCount != this.CurrentPage)
+            {
+                this.paginacion.nextPage();
+                this.LoadMarca();
+            }
         }
 
         private void ExecutelastPage()
         {
-            throw new NotImplementedException();
+            if (this.paginacion.pageCount != this.CurrentPage)
+            {
+                this.paginacion.lastPage();
+                this.LoadMarca();
+            }
         }
         #endregion
 
         #region ===================================== LOADS =====================================
-        private async void LoadMarca(int page, int items)
+        private async void LoadMarca()
         {
             try
             {
-                IsRefreshing = true;
+                this.IsRefreshing = true;
                 // www.lineatienda.com/services.php/marcas/estado/1/100
-                RootObject<Marca> rootData = await webService.GET<RootObject<Marca>>("marcas", String.Format("estado/{0}/{1}", page, items));
-                marcaList = rootData.datos;
+                RootObject<Marca> rootData = await webService.GET<RootObject<Marca>>("marcas", String.Format("estado/{0}/{1}", paginacion.currentPage, App.configuracionGeneral.itemPorPagina));
+                this.marcaList = rootData.datos;
 
+                // Set data paginacion
+                this.paginacion.itemsCount = rootData.nro_registros;
+                this.paginacion.reload();
+
+                // Reload pagination
+                this.reloadPagination();
+
+                // create observablecollection
                 this.MarcaItems = new ObservableCollection<MarcaItemViewModel>(
                     this.ToMarcaItemViewModel());
             }
@@ -147,8 +218,21 @@ namespace AdmeliApp.ViewModel
             }
             finally
             {
-                IsRefreshing = false;
+                this.IsRefreshing = false;
             }
+        }
+
+        private void reloadPagination()
+        {
+            // Print data pagination
+            this.CurrentPage = paginacion.currentPage;
+            this.NRegistros = String.Format("{0} Registros de {1} ", App.configuracionGeneral.itemPorPagina, this.paginacion.itemsCount);
+
+            // Pagination button navigation is visible false or true
+            this.NextIsVisible = ((this.paginacion.pageCount - this.paginacion.currentPage) >= 1);
+            this.LastIsVisible = ((this.paginacion.pageCount - this.paginacion.currentPage) >= 2);
+            this.PreviousIsVisible = (this.paginacion.currentPage >= 2);
+            this.FirstIsVisible = (this.paginacion.currentPage >= 3);
         }
 
         internal void SetCurrentMarca(MarcaItemViewModel marcaItemViewModel)
@@ -182,11 +266,11 @@ namespace AdmeliApp.ViewModel
 
         public static MarcaViewModel GetInstance()
         {
-            if (instance == null)
+            if (MarcaViewModel.instance == null)
             {
                 return new MarcaViewModel();
             }
-            return instance;
+            return MarcaViewModel.instance;
         }
         #endregion
     }

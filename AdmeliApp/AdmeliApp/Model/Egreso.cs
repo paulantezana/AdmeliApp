5dd5d5bde6c4f18c908dfa9db040a91f6fe97cdf
paul antezana <contacto@paulantezana.com>
2018-04-26 19:01:55 -0500

    new updates

---
+++ AdmeliApp/AdmeliApp/Model/Egreso.cs
@@ -0,0 +1,42 @@
+﻿using AdmeliApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdmeliApp.Model
+{
+    public class Egreso : BaseModel
+    {
+        public int idEgreso { get; set; }
+        public string numeroOperacion { get; set; }
+        public Fecha fecha { get; set; }
+        public Fecha fechaPago { get; set; }
+        public string monto { get; set; }
+        public string motivo { get; set; }
+        public string observacion { get; set; }
+        public string moneda { get; set; }
+        public int estado { get; set; }
+        public int idMoneda { get; set; }
+        public int idCaja { get; set; }
+        public int idCajaSesion { get; set; }
+        public int idDetallePago { get; set; }
+        public int idMedioPago { get; set; }
+        public string medioPago { get; set; }
+        public string personal { get; set; }
+        public string esDeCompra { get; set; }
+
+        private string estadoString;
+        public string EstadoString
+        {
+            get
+            {
+                if (estado == 1) { return "Activo"; }
+                else { return "Anulado"; }
+            }
+            set
+            {
+                estadoString = value;
+            }
+        }
+    }
+}
---
+++ AdmeliApp/AdmeliApp/Model/Ingreso.cs
@@ -0,0 +1,46 @@
+﻿using AdmeliApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdmeliApp.Model
+{
+    public class Ingreso : BaseModel
+    {
+        public int idIngreso { get; set; }
+        public string numeroOperacion { get; set; }
+        public Fecha fecha { get; set; }
+        public Fecha fechaPago { get; set; }
+        public string monto { get; set; }
+        public string motivo { get; set; }
+        public string observacion { get; set; }
+        public int estado { get; set; }
+        public string moneda { get; set; }
+        public int idMoneda { get; set; }
+        public int idCajaSesion { get; set; }
+        public int idDetalleCobro { get; set; }
+        public int idMedioPago { get; set; }
+        public string medioPago { get; set; }
+        public string esDeVenta { get; set; }
+        public string personal { get; set; }
+        public int idAsignarCaja { get; set; }
+        public string montoCierre { get; set; }
+        public object fechaCierre { get; set; }
+
+        public string[] valores { get; set; }
+
+        private string estadoString;
+        public string EstadoString
+        {
+            get
+            {
+                if (estado == 1) { return "Activo"; }
+                else { return "Anulado"; }
+            }
+            set
+            {
+                estadoString = value;
+            }
+        }
+    }
+}
